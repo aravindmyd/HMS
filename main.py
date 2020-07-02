a3072b9a79797fc7354ebc2015bea36484511bb0
aravindmyd <aravindmyd@gmail.com>
2020-07-02 18:26:20 +0530

    Flash Added:

--- main.py
+++ main.py
@@ -1,61 +1,54 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
 from datetime import datetime
 
 now = datetime.now()
 app = Flask(__name__)
-# Change this to your secret key (can be anything, it's for extra protection)
+
 app.secret_key = 'sdfasdfasdbasfbhadsbfjasdnfjasdufh asdkfn'
-# Enter your database connection details below
+
+#Database Details
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'testpassword'
 app.config['MYSQL_DB'] = 'HMS'
-# Initialize MySQL
+
 mysql = MySQL(app)
 
-
-# http://localhost:5000/login/ - this will be the login page, we need to use both GET and POST requests
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         option = request.form['holder']
 
-        # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(f'SELECT * FROM {option} WHERE username = %s AND password = %s', (username, password,))
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-        # Fetch one record and return result
         account = cursor.fetchone()
-        # If account exists in accounts table in out database
         if account:
-            # Create session data, we can access this data in other routes
             cursor.execute(f"UPDATE {option} SET timestamp = '{str(date_time)}' WHERE id={account['id']} ")
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
-            # Redirect to home page
+
+            msg = "Logged in Succesfully!"
             if option == "admission":
+                flash(msg)
                 return render_template('admissionHomePage.html')
             elif option == "pharmacist":
+                flash(msg)
                 return render_template('pharmacistHomePage.html')
             elif option == "diagnostic":
+                flash(msg)
                 return render_template('diagnosticHomePage.html')
 
         else:
-            # Account doesnt exist or username/password incorrect
-            flash(msg)
             msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
+            flash(msg)
     return render_template('login.html', msg=msg)
 
 
